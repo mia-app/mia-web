c180dc0a3d17da5b13996721a957e304d9d20e3c
Gabriel Bénédict <gbndict@gmail.com>
2020-04-04 15:39:34 +0200

    json tree

---
+++ miniapp/build/precache-manifest.a0f990e3679c054d983822165ef46bd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c8ef90018dd323b42d6cee26490211",
+    "url": "/sick-bot.github.io/index.html"
+  },
+  {
+    "revision": "220732aaa3123d6bbd22",
+    "url": "/sick-bot.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9a2d9a6c45dc0f29e327",
+    "url": "/sick-bot.github.io/static/js/2.0fe8fb2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sick-bot.github.io/static/js/2.0fe8fb2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220732aaa3123d6bbd22",
+    "url": "/sick-bot.github.io/static/js/main.535ef2a5.chunk.js"
+  },
+  {
+    "revision": "02c0cf1f32888021504f",
+    "url": "/sick-bot.github.io/static/js/runtime-main.62c66cbb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sick-bot.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
