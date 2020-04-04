fbf75ef65706fc3d3b07308f5b3fbb6e57ce1192
oliwalker <oliver@ginetta.net>
2020-04-04 22:36:46 +0200

    minor touch ups

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,19 +4,21 @@ import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <div className="home">
-      <h1 className="home__header">Hi,</h1>
-      <h1 className="home__header">I'm Mia</h1>
-      <p className="home__text1">
-        I can help if you think you may be having symptoms of Corona and you
-        need to tell the others around you.
-      </p>
+      <div className="home__landing">
+        <h1 className="home__header">Hi,</h1>
+        <h1 className="home__header">I'm Mia</h1>
+        <p className="home__text1">
+          I can help if you think you may be having symptoms of Corona and you
+          need to tell the others around you.
+        </p>
 
-      <p className="home__text2">For now I am just a prototype</p>
-      <p className="home__text2">~ Be easy on me! ~</p>
+        <p className="home__text2">For now I am just a prototype</p>
+        <p className="home__text2">~ Be easy on me! ~</p>
 
-      <span role="img" aria-label="Mia" className="home__mia">
-        🙋‍♀️
-      </span>
+        <span role="img" aria-label="Mia" className="home__mia">
+          🙋‍♀️
+        </span>
+      </div>
 
       <div className="home__buttons">
         <Link to="/chat-with-mia" className="button">
