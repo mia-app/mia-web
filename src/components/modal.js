d7a60e617dbeb80f5f1cb0cf422025698c543aa2
oliwalker <oliver@ginetta.net>
2020-04-05 13:10:06 +0200

    added modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export const Modal = ({ title, body, link, href, error }) => {
   return (
-    <div className={error ? "modal error" : "modal"}>
+    <motion.div
+      className={error ? "modal error" : "modal"}
+      initial={{ opacity: 0, y: -30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.5,
+      }}
+    >
       <h3 className={error ? "error" : ""}>{title}</h3>
       <p className={error ? "error" : ""}>{body}</p>
       <a href={href} className="link" target="_blank" rel="noopener noreferrer">
         {link}
       </a>
-    </div>
+    </motion.div>
   );
 };
